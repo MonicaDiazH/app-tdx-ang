618c18a8e0e28b2f1517b345a9fd75c298d0c212
Monica Diaz <monica.diaz@ingeneo.com.co>
2024-03-07 11:18:14 -0600

    Cardnumber encryption.

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +13,15 @@ export class PaymentService {
   processPayment(cardNumber: string, delaySeconds: number): Observable<any> {
     const apiUrl = this.baseUrl + delaySeconds;
     const startTime = performance.now();
+    const encryptedData = this.encryptData({ cardNumber });
 
     return new Observable(observer => {
-      this.http.post(apiUrl, { cardNumber }).subscribe(
+      this.http.post(apiUrl, { encryptedData }).subscribe(
         response => {
           const endTime = performance.now();
           const responseTime = endTime - startTime;
           observer.next(responseTime);
+          console.log(response);
           observer.complete();
           console.log('Tiempo de respuesta:', responseTime, 'milisegundos');
         },
@@ -27,5 +30,14 @@ export class PaymentService {
         }
       );
     });
+  }
+
+  private encryptData(data: any): any {
+    const key = CryptoJS.enc.Utf8.parse('1sre6-sdfe26-99www');
+    const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), key, {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7
+    });
+    return encryptedData.toString();
   }
 }
